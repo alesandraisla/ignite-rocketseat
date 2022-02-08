bfdc9635af1076f7546241d1062c906c97548182
AlesandraIsla <alesandramempis@gmail.com>
2022-02-08 16:49:03 -0300

    Inclusão do scrip de desenvolvimento e de producao

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -5,12 +5,20 @@
 // output  arquivo que vai gerar o webpack
 // resolve pega as extencoes que podem ser lidas 
 //module configuracoes de como a app vai se comportar de acordo com cada arquivo  
-//mode é o modo de desenvolvimento que pode ser development ou production   
+//mode é o modo de desenvolvimento que pode ser development ou production  
+//pluguins o html_webpack_plugin serva para exportar um novo html para a pasta dist
+// devServer serve para ficar observando cada atualizacao que o desenvolverdor realizar  
+// devtool -> serve para  conseguir visualizar o cod da pagina original no source debuggar melhor
 
 const path = require('path') //usa dessa forma com require porque ele usa o nodejs
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+//variaveis de ambientes 
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -19,6 +27,14 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
