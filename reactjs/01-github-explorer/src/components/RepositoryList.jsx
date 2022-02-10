3fbd1653ed3fdc2bd11e8c3d5099a6e55672ede0
AlesandraIsla <alesandramempis@gmail.com>
2022-02-10 10:28:51 -0300

    Inclusao da API github e useEffect

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,22 +1,33 @@
 // const repositoryName = 'uniform2';
 
 import { RepositoryItem } from "./RepositoryItem";
+import { useState } from 'react';
+import '../styles/repositories.scss';
+import { useEffect } from "react";
 
-const repository = {
-    name: 'uniform',
-    descripton: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+// estática
+// const repository = {
+//     name: 'uniform',
+//     descripton: 'Forms in React',
+//     link: 'https://github.com/unform/unform'
+// }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="Repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />  
-                <RepositoryItem repository={repository} />   
-                <RepositoryItem repository={repository} />   
-                <RepositoryItem repository={repository} />    
+                {repositories.map(repository => {
+                    return <RepositoryItem repository={repository}/>
+                })}
             </ul>
         </section>
     )
